611969e709f13cad2534d240945ae48aab408ee6
Awoyo@20 <awoyooluwadamola@gmail.com>
2022-05-19 06:01:10 -0700

    update

--- grade2.js
+++ grade2.js
@@ -43,3 +43,20 @@ console.log(score_70);
 console.log(score_60);
 console.log(score_50);
 console.log(score_49);
+
+let studentScore = prompt("What is your score?")
+let studentName = prompt("What is your Name?")
+
+if(studentScore >=80 && studentScore <=100){
+console.log('${studentName}, your score is ${studentScore}and you have B')
+} else if(studentScore >=70 && studentScore <80){
+    console.log('${studentName}, your score is ${studentScore}and you have A')
+}else if(studentScore >=60 && studentScore <70){
+    console.log('${studentName}, your score is ${studentScore}and you have C')
+} else if(studentScore >=50 && studentScore <60){
+    console.log('${studentName}, your score is ${studentScore}and you have D')
+} else if(studentScore <50){
+    console.log('${studentName}, your score is ${studentScore}and you have f')
+} else{
+    console.log('${studentName}, you must be a thief to type an invalid score')
+}
--- month.js
+++ month.js
@@ -1,10 +1,97 @@
-// get days in month
-var getDaysInMonth =function(month,year){
-    // Here January is 1 based 
-    //Day 0 is the last day in the previous month
-    return new Date(year, month, 0).getDate();
-    //Here January is 0 based
-    // return new Date (year, month+1, 0).getDate();
-};
-console.log(getDaysInMonth(1, 2022));
-console.log(getDaysInMonth(2, 2022));
+// // get days in month
+// var getDaysInMonth =function(month,year){
+//     // Here January is 1 based 
+//     //Day 0 is the last day in the previous month
+//     return new Date(year, month, 0).getDate();
+//     //Here January is 0 based
+//     // return new Date (year, month+1, 0).getDate();
+// };
+// console.log(getDaysInMonth(1, 2022));
+// console.log(getDaysInMonth(2, 2022));
+
+
+// let months = new Date(new Date().getMonth()) +1
+// let checkDayInaMonth = prompt("Enter a month")
+
+// if(months ==1){
+// }
+// let typeMonth = prompt("Entetr a month")
+// if (typeMonth == "September" || typeMonth =="June"  || typeMonth == "April" || typeMonth == "November") {
+//     console.log('${typeMonth} has 30 days' )
+// }else if (typeMonth == "January" || typeMonth =="May"  || typeMonth == "July" || typeMonth == "August" || typeMonth== "October" || typeMonth== "December") {
+//     console.log('${typeMonth} has 31 days' )
+// }else if(typeMonth == "Feburary"){
+//     console.log('${typeMonth}has 28 days')
+//     }else ( "g") 
+
+    
+
+    
+
+
+    
+    
+    // let joke = 34;
+    // let tade = 25;
+    // let lolu = 46;
+
+    // if(joke > tade && joke > lolu){
+    //     console.log('${joke} is the biggest number')
+    // }else if ( tade > joke && tade > lolu){
+    //     console.log('${tade} is the biggest number')
+
+    // } else {
+    //     console.log("46")
+    // }
+
+     //for loop
+     
+     
+    //  for(condition) {
+
+    // }for(let i = 0; i <=5; i++){
+    //     console.log(i)
+    //  }
+      
+
+//     for( let a = 0; a <=100; a++) {
+//         console.log(a)
+//      if(a%2 == 0) {
+//         console.log( "  even  number");   
+// }
+// else {
+//         console.log(" odd number");
+// }
+//     }
+
+    for( let b = 0; b <=5; b++){
+
+    } if ( 0 == "0*0=0"){
+        console.log(parseInt("0"));
+    }
+      else if ( 0 == "1*1=1"){
+        console.log(parseInt("1"));
+    }
+      else if ( 0 == "2*2=4"){
+        console.log(parseInt("4"));
+    }
+      else if ( 0 == "3*3=9"){
+        console.log(parseInt("9"));
+    }
+     else if ( 0 == "4*4=16"){
+        console.log( parseInt("16"));
+    }
+     else ( 0 == "5*5=25") 
+        console.log(parseInt("25"));
+
+
+        for (  let k= 0; k<=5; k++){
+           console.log (`${k} * ${k} = ${k*k}`)
+        }
+ 
+        const data =[1,2,3,4,5,6,7,8,9,10]
+        let sum = 0
+        for( let sum = 0; sum<data.length; sum++){
+sum= sum + data[sum]
+            console.log(sum) ;
+        }
